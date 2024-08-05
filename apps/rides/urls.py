f3bab0b9feefe67622417da3820baa68ee8190d7
RadySonabu <ardynubanos@gmail.com>
2024-08-06 04:33:58 +0800

    add: basename for rides urls

--- apps/rides/urls.py
+++ apps/rides/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import RideViewSet, RideEventViewSet
 
 rides_router = DefaultRouter()
-rides_router.register(r"rides", RideViewSet)
-rides_router.register(r"rides-events", RideEventViewSet)
+rides_router.register(r"rides", RideViewSet, "rides")
+rides_router.register(r"rides-events", RideEventViewSet, "rides-events")
 
 urlpatterns = [
     path("api/rides/", include(rides_router.urls)),
